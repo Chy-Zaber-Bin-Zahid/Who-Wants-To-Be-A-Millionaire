5fd891223435dbda24ef6ed005fb4ba962c0c151
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2023-09-24 23:43:09 +0600

    css done for now

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Icon from "./components/icon";
 import Question from "./components/question";
 import Prize from "./components/prize";
 import Timer from "./components/timer";
+import Option from "./components/option";
 
 function App() {
   let audio = new Audio("/sound/intro.mp3")
@@ -18,6 +19,7 @@ function App() {
         </div>
         <div className="right-column">
           <Prize />
+          <Option/>
         </div>
       </div>
     </>
---
+++ src/components/option.jsx
@@ -0,0 +1,15 @@
+export default function Option() {
+  return (
+    <div className="option">
+      <div className="option-three">
+        <button title="50:50">50:50</button>
+        <button title="Audience">👨‍👧‍👦</button>
+        <button title="Call">📞</button>
+      </div>
+      <div className="option-three">
+        <button>Start</button>
+      </div>
+      <p className="creator">Made By Chowdhury Zaber Bin Zahid | 2023</p>
+    </div>
+  );
+}
