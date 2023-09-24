2e8ae662ea539cad3bced1dfbbb27cf73d90df45
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2023-09-24 21:34:24 +0600

    prize component css done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React from "react";
 import Icon from "./components/icon";
-import Question from './components/question';
-import Prize from './components/prize';
-import Timer from './components/timer';
+import Question from "./components/question";
+import Prize from "./components/prize";
+import Timer from "./components/timer";
 
 function App() {
+  let audio = new Audio("/sound/intro.mp3")
+  audio.play()
+
   return (
-    <div className="app">
-      <div className='left-column'>
-        <Icon />
-        <Timer />
-        <Question/>
+    <>
+      <div className="app">
+        <div className="left-column">
+          <Icon />
+          <Timer />
+          <Question />
+        </div>
+        <div className="right-column">
+          <Prize />
+        </div>
       </div>
-      <Prize/>
-    </div>
+    </>
   );
 }
 
--- src/components/prize.jsx
+++ src/components/prize.jsx
@@ -1,6 +1,23 @@
 export default function Prize() {
   return (
-    <>
-    </>
+    <div className="prize">
+      <div>
+        <h1 className="prize-text prize-save">15 $ 1 000 000</h1>
+        <h1 className="prize-text">14 $ 500 000</h1>
+        <h1 className="prize-text">13 $ 250 000</h1>
+        <h1 className="prize-text">12 $ 125 00</h1>
+        <h1 className="prize-text">11 $ 64 000</h1>
+        <h1 className="prize-text prize-save">10 $ 32 000</h1>
+        <h1 className="prize-text">9 &nbsp;&nbsp;$ 16 000</h1>
+        <h1 className="prize-text">8 &nbsp;&nbsp;$ 8 000</h1>
+        <h1 className="prize-text">7 &nbsp;&nbsp;$ 4 000</h1>
+        <h1 className="prize-text">6 &nbsp;&nbsp;$ 2 000</h1>
+        <h1 className="prize-text prize-save">5 &nbsp;&nbsp;$ 1 000</h1>
+        <h1 className="prize-text">4 &nbsp;&nbsp;$ 500</h1>
+        <h1 className="prize-text">3 &nbsp;&nbsp;$ 300</h1>
+        <h1 className="prize-text">2 &nbsp;&nbsp;$ 200</h1>
+        <h1 className="prize-text prize-running">1 &nbsp;&nbsp;$ 100</h1>
+      </div>
+    </div>
   );
 }
