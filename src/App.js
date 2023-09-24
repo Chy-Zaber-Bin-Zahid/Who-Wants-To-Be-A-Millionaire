a0b300e23bf2996a61feaaf5ea6fa26a398dbfeb
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2023-09-24 19:36:56 +0600

    componnent created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
+import React from 'react';
+import Icon from "./components/icon";
+import Question from './components/question';
+import Prize from './components/prize';
+import Timer from './components/timer';
+
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className='left-column'>
+        <Icon />
+        <Timer />
+        <Question/>
+      </div>
+      <Prize/>
     </div>
   );
 }
---
+++ src/components/icon.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Icon() {
+  return (
+    <>
+      <img className="logo" src="../../public/img/logo.png" alt="Millionaire Logo" />
+    </>
+  );
+}
