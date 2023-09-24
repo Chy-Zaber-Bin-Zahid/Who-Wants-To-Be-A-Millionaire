9c3cc067133666f4a5bd06a6221b1afd1aca5f98
Chy-Zaber-Bin-Zahid <89964035+Chy-Zaber-Bin-Zahid@users.noreply.github.com>
2023-09-24 20:36:15 +0600

    question component css done

--- src/components/question.jsx
+++ src/components/question.jsx
@@ -1,4 +1,9 @@
 export default function Question(){
-  return <>
-  </>
+  return <div className="question">
+    <h1 className="main-question">Question</h1>
+    <button className="answer">lorem10</button>
+    <button className="answer">lorem10</button>
+    <button className="answer">lorem10</button>
+    <button className="answer">lorem10</button>
+  </div>
 }
